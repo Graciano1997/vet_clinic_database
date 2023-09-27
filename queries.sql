db254cd1f82d3b0894c9eff6a2fb468fc064e7bd
Graciano1997 <gracianomanuelhenrique@gmail.com>
2023-09-27 11:24:14 +0100

    add option change

--- queries.sql
+++ queries.sql
@@ -6,5 +6,5 @@ SELECT * FROM animals WHERE neutered=true AND escape_attempts<3;
 SELECT date_of_birth FROM animals WHERE name='Agumon' OR name='Pikachu';
 SELECT name,escape_attempts FROM animals WHERE weight_kg>10.5;
 SELECT * FROM animals WHERE neutered=true;
-SELECT * FROM animals WHERE name NOT LIKE '%Gabumon%';
+SELECT * FROM animals WHERE name <> 'Gabumon';
 SELECT * FROM animals WHERE weight_kg<=17.3 AND weight_kg>=10.4;
