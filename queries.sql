a3c8d1309c461135c8be0d3e2c36d0eebe095da2
Graciano1997 <gracianomanuelhenrique@gmail.com>
2023-09-28 01:09:02 +0100

    add other transations

--- queries.sql
+++ queries.sql
@@ -8,3 +8,56 @@ SELECT name,escape_attempts FROM animals WHERE weight_kg>10.5;
 SELECT * FROM animals WHERE neutered=true;
 SELECT * FROM animals WHERE name <> 'Gabumon';
 SELECT * FROM animals WHERE weight_kg<=17.3 AND weight_kg>=10.4;
+
+/* First Transation*/
+BEGIN WORK;
+ALTER TABLE animals 
+RENAME COLUMN species TO unspecified;
+SELECT * FROM animals;
+ROLLBACK;
+END;
+
+/* SECOND Transation*/
+
+BEGIN;
+UPDATE animals
+SET species='digimon'
+WHERE name like '%mon';
+
+UPDATE animals
+SET species='pokemon'
+WHERE species IS NULL;
+select * from animals;
+COMMIT;
+END;
+select * from animals;
+
+/*THIRD TRANSATION*/
+
+BEGIN;
+DELETE FROM animals;
+select * from animals;
+rollback;
+END;
+select * from animals;
+
+/*fourth Transation*/
+
+BEGIN;
+
+DELETE FROM animals where date_of_birth > '2022-01-01';
+
+SAVEPOINT deletePoint2022;
+
+UPDATE animals
+SET weight_kg=weight_kg*-1;
+
+ROLLBACK TO deletePoint2022;
+
+UPDATE animals
+SET weight_kg=weight_kg*-1
+where weight_kg<0;
+
+COMMIT;
+
+END;
