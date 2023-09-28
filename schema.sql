442f5f6bd9e77efa24defb63fb4d37211bee539e
Graciano1997 <gracianomanuelhenrique@gmail.com>
2023-09-28 19:10:21 +0100

    add fk in animals

--- schema.sql
+++ schema.sql
@@ -1,8 +1,8 @@
 /* Database schema to keep the structure of entire database. */
-CREATE DATABASE vet_clinic; /*only if not exists*/
+CREATE DATABASE vet_clinic;
 
 CREATE TABLE IF NOT EXISTS  animals (
-	id INTEGER GENERATED ALWAYS AS IDENTITY,
+	id SERIAL PRIMARY KEY,
 	name VARCHAR(100),
 	date_of_birth DATE,
 	escape_attempts INTEGER,
@@ -21,3 +21,20 @@ CREATE TABLE IF NOT EXISTS species(
 id SERIAL PRIMARY KEY,
 name VARCHAR(200)
 );
+
+alter table animals
+DROP COLUMN IF EXISTS  species,
+ADD COLUMN IF NOT EXISTS species_id int,
+ADD COLUMN IF NOT EXISTS owner_id int;
+
+ALTER TABLE animals
+ADD CONSTRAINT fk_species FOREIGN KEY (species_id) REFERENCES species(id),
+ADD CONSTRAINT fk_owner FOREIGN KEY (owner_id) REFERENCES owners(id);
+
+ALTER TABLE animals
+ADD CONSTRAINT fk_species
+FOREIGN KEY(species_id)
+REFERENCES species(id),
+ADD CONSTRAINT fs_owner
+FOREIGN KEY(owner_id)
+REFERENCES owners(id);
